08863912ed260edc93be6a140f5a43bc97785afe
haruki7049 <tontonkirikiri@gmail.com>
2024-08-31 19:41:30 +0900

    feat(flake-utils): Init

--- flake.nix
+++ flake.nix
@@ -3,55 +3,50 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     vim-src = {
       url = "github:vim/vim";
       flake = false;
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    vim-src,
-  }: let
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-
-    vim-overlay = final: prev: {
-      vim = prev.vim.overrideAttrs (oldAttrs: {
-        version = "latest";
-        src = vim-src;
-        configureFlags =
-          oldAttrs.configureFlags
-          ++ [
-            "--enable-terminal"
-            "--with-compiledby=vim-overlay"
-            "--enable-luainterp"
-            "--with-lua-prefix=${prev.lua}"
-            "--enable-fail-if-missing"
-          ];
-        buildInputs =
-          oldAttrs.buildInputs
-          ++ [prev.gettext prev.lua prev.libiconv];
-      });
-    };
-  in {
-    overlays.default = vim-overlay;
-
-    packages = nixpkgs.lib.genAttrs systems (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [vim-overlay];
+  outputs = { nixpkgs, vim-src, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        vim-overlay = final: prev: {
+          vim = prev.vim.overrideAttrs (oldAttrs: {
+            version = "latest";
+            src = vim-src;
+            configureFlags =
+              oldAttrs.configureFlags
+              ++ [
+                "--enable-terminal"
+                "--with-compiledby=vim-overlay"
+                "--enable-luainterp"
+                "--with-lua-prefix=${prev.lua}"
+                "--enable-fail-if-missing"
+              ];
+            buildInputs =
+              oldAttrs.buildInputs
+              ++ [prev.gettext prev.lua prev.libiconv];
+          });
         };
-      in {
-        vim = pkgs.vim;
-        default = pkgs.vim;
-      }
-    );
-  };
+      in
+      {
+        overlays.default = vim-overlay;
+
+        packages =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              vim-overlay
+            ];
+          };
+        in
+        {
+          vim = pkgs.vim;
+          default = pkgs.vim;
+        };
+      });
 }
